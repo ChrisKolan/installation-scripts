e4682a20308490d0ea1ba55b7ded8593db04b152
Chris Kolan <chris.kolann@gmail.com>
2025-01-04 19:59:16 +0100

    Modified starship

--- scripts/install-starship.sh
+++ scripts/install-starship.sh
@@ -2,3 +2,9 @@
 # https://github.com/starship/starship?tab=readme-ov-file#step-1-install-starship
 
 curl -sS https://starship.rs/install.sh | sh
+
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+
+source ~/.bashrc
+source ~/.zshrc
