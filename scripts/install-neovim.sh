fbf72d16faeb725a955c8c97a85aa73b7403279d
Chris Kolan <chris.kolann@gmail.com>
2025-01-11 18:13:24 +0100

    Update install-neovim.sh

--- scripts/install-neovim.sh
+++ scripts/install-neovim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # https://github.com/neovim/neovim/blob/master/INSTALL.md#linux
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz &&
-sudo rm -rf /opt/nvim &&
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
 EXPORT_PATH='export PATH="$PATH:/opt/nvim-linux64/bin"' 
