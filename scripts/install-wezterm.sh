bf3b78a7c88718592d045309fed01bc61b4b87b0
Chris Kolan <chris.kolann@gmail.com>
2025-01-11 12:53:06 +0100

    Update install-wezterm.sh

--- scripts/install-wezterm.sh
+++ scripts/install-wezterm.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # https://wezfurlong.org/wezterm/install/linux.html#__tabbed_1_3
 
-sudo mkdir /etc/apt/keyrings
-
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 
