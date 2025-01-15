421f1ff8c522214c410cd0c624a4d950a9bb114f
Chris Kolan <chris.kolann@gmail.com>
2025-01-15 22:35:04 +0100

    improvements

--- shared-scripts/install-homebrew-packages.sh
+++ shared-scripts/install-homebrew-packages.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+brew install gcc
+brew install hello
 brew install starship
 brew install sshs
 brew install syncthing
--- shared-scripts/install-homebrew.sh
+++ shared-scripts/install-homebrew.sh
@@ -2,3 +2,6 @@
 # https://brew.sh/
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> /home/chris/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/chris/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
