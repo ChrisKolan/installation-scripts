c16b01e97c2937eca442379d440e8ba744add47a
Chris Kolan <chris.kolann@gmail.com>
2025-01-16 17:31:19 +0100

    Picard

--- shared-scripts/install-flatpaks.sh
+++ shared-scripts/install-flatpaks.sh
@@ -4,4 +4,4 @@ flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flath
 flatpak install --user -y flathub info.portfolio_performance.PortfolioPerformance
 flatpak install --user -y flathub org.freeciv.gtk322
 flatpak install --user -y flathub org.mozilla.firefox
-flatpak install --user -y flathub org.musicbrainz.Picar
+flatpak install --user -y flathub org.musicbrainz.Picard
