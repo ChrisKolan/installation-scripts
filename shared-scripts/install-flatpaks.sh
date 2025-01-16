99118c710e610b6a5aa96938256ef8db4ef4f622
Chris Kolan <chris.kolann@gmail.com>
2025-01-16 11:46:37 +0100

    flatpak install --user

--- shared-scripts/install-flatpaks.sh
+++ shared-scripts/install-flatpaks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-flatpak install -y flathub info.portfolio_performance.PortfolioPerformance
-flatpak install -y flathub org.freeciv.gtk322
-flatpak install -y flathub org.mozilla.firefox
-flatpak install -y flathub org.musicbrainz.Picard
+flatpak install --user -y flathub info.portfolio_performance.PortfolioPerformance
+flatpak install --user -y flathub org.freeciv.gtk322
+flatpak install --user -y flathub org.mozilla.firefox
+flatpak install --user -y flathub org.musicbrainz.Picar
