9e6431381e6eca98e37cf5834804e30ffe9b4246
Chris Kolan <chris.kolann@gmail.com>
2025-01-16 12:14:11 +0100

    flatpak remote-add --user

--- shared-scripts/install-flatpaks.sh
+++ shared-scripts/install-flatpaks.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install --user -y flathub info.portfolio_performance.PortfolioPerformance
 flatpak install --user -y flathub org.freeciv.gtk322
 flatpak install --user -y flathub org.mozilla.firefox
