0b4b85964f241e2fb4d72d993040ee1bb80dbaa9
Chris Kolan <chris.kolann@gmail.com>
2025-01-16 19:34:37 +0100

    use Homebrew temporary and immediately

--- shared-scripts/install-homebrew-packages.sh
+++ shared-scripts/install-homebrew-packages.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# To use Homebrew temporary and immediately in the current terminal session, run:
+export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+
 brew install gcc
 brew install hello
 brew install starship
