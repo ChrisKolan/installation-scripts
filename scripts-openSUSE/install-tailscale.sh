a5a30dbad54628cf765ab39107045850172171f7
Chris Kolan <chris.kolann@gmail.com>
2025-01-16 20:42:20 +0100

    removed zypper addrepo

--- scripts-openSUSE/install-tailscale.sh
+++ scripts-openSUSE/install-tailscale.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # https://software.opensuse.org/download/package?package=tailscale&project=openSUSE%3AFactory
 
-sudo zypper addrepo https://download.opensuse.org/repositories/openSUSE:Factory/standard/openSUSE:Factory.repo
-sudo zypper refresh
 sudo zypper install -y tailscale
 
 sudo systemctl enable tailscaled
