0a7094bc7c63cdf964ae4796efca86bb96062df3
Chris Kolan <chris.kolann@gmail.com>
2025-01-19 19:05:17 +0100

    Update install-flatpaks.sh
    added music player

--- shared-scripts/install-flatpaks.sh
+++ shared-scripts/install-flatpaks.sh
@@ -5,3 +5,4 @@ flatpak install --user -y flathub info.portfolio_performance.PortfolioPerformanc
 flatpak install --user -y flathub org.freeciv.gtk322
 flatpak install --user -y flathub org.mozilla.firefox
 flatpak install --user -y flathub org.musicbrainz.Picard
+flatpak install --user -y flathub org.fooyin.fooyin
