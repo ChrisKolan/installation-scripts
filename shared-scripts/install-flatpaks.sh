ef9584f8ac35f4261e29663bbb9b47667f9a3f73
Chris Kolan <chris.kolann@gmail.com>
2025-01-23 19:00:46 +0100

    Update install-flatpaks.sh
    disabled firefox

--- shared-scripts/install-flatpaks.sh
+++ shared-scripts/install-flatpaks.sh
@@ -3,6 +3,6 @@
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install --user -y flathub info.portfolio_performance.PortfolioPerformance
 flatpak install --user -y flathub org.freeciv.gtk322
-flatpak install --user -y flathub org.mozilla.firefox
+# flatpak install --user -y flathub org.mozilla.firefox
 flatpak install --user -y flathub org.musicbrainz.Picard
 flatpak install --user -y flathub org.fooyin.fooyin
