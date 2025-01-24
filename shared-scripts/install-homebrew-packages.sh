f257041814fb646e52625cbe26151d3075ef6281
Chris Kolan <chris.kolann@gmail.com>
2025-01-24 22:02:26 +0100

    deactivated brew install gcc

--- shared-scripts/install-homebrew-packages.sh
+++ shared-scripts/install-homebrew-packages.sh
@@ -3,7 +3,7 @@
 # To use Homebrew temporary and immediately in the current terminal session, run:
 export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 
-brew install gcc
+#brew install gcc
 brew install hello
 brew install sshs
 #brew install rclone
