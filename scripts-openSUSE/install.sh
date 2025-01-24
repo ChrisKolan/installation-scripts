9b4d60b3c4bc41a951179d49a5d6b625112e79f2
Chris Kolan <chris.kolann@gmail.com>
2025-01-24 21:15:55 +0100

    disabled stuff for testing

--- scripts-openSUSE/install.sh
+++ scripts-openSUSE/install.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-echo "Invoking pre-installation-script"
-./shared-scripts/operation-date-time.sh
-./scripts-openSUSE/pre-installation-script.sh
+# echo "Invoking pre-installation-script"
+# ./shared-scripts/operation-date-time.sh
+# ./scripts-openSUSE/pre-installation-script.sh
 
-echo "Installing System Packages"
-./shared-scripts/operation-date-time.sh
-sudo xargs -a package-list/packages-to-install-openSUSE.txt zypper install -y
+# echo "Installing System Packages"
+# ./shared-scripts/operation-date-time.sh
+# sudo xargs -a package-list/packages-to-install-openSUSE.txt zypper install -y
 
-echo "Installing Fonts"
-./shared-scripts/operation-date-time.sh
-./shared-scripts/install-fonts.sh
+# echo "Installing Fonts"
+# ./shared-scripts/operation-date-time.sh
+# ./shared-scripts/install-fonts.sh
 
-echo "Installing Syncthing"
-./shared-scripts/operation-date-time.sh
-./scripts-openSUSE/install-syncthing.sh
+# echo "Installing Syncthing"
+# ./shared-scripts/operation-date-time.sh
+# ./scripts-openSUSE/install-syncthing.sh
 
-echo "Installing Tailscale"
-./shared-scripts/operation-date-time.sh
-./scripts-openSUSE/install-tailscale.sh
+# echo "Installing Tailscale"
+# ./shared-scripts/operation-date-time.sh
+# ./scripts-openSUSE/install-tailscale.sh
 
-echo "Installing Rclone"
-./shared-scripts/operation-date-time.sh
-./scripts-openSUSE/install-rclone.sh
+# echo "Installing Rclone"
+# ./shared-scripts/operation-date-time.sh
+# ./scripts-openSUSE/install-rclone.sh
 
-echo "Installing homebrew packages"
-./shared-scripts/operation-date-time.sh
-./shared-scripts/install-homebrew-packages.sh
+# echo "Installing homebrew packages"
+# ./shared-scripts/operation-date-time.sh
+# ./shared-scripts/install-homebrew-packages.sh
 
-echo "Installing Visual Studio Code"
-./shared-scripts/operation-date-time.sh
-./scripts-openSUSE/install-vs-code.sh
+# echo "Installing Visual Studio Code"
+# ./shared-scripts/operation-date-time.sh
+# ./scripts-openSUSE/install-vs-code.sh
 
 echo "Installing Chezmoi"
 ./shared-scripts/operation-date-time.sh
