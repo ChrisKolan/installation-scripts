32e59871f11b98a5759e0a2a160021096ef7e2d4
chris <chris.kolann@gmail.com>
2025-02-01 12:25:15 +0100

    updated appimages

--- shared-scripts/get-app-images.sh
+++ shared-scripts/get-app-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ~/bin
-wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.7.7/Obsidian-1.7.7.AppImage
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.8.4/Obsidian-1.8.4.AppImage
 wget https://github.com/keepassxreboot/keepassxc/releases/download/2.7.9/KeePassXC-2.7.9-x86_64.AppImage
-wget https://github.com/probonopd/go-appimage/releases/download/832/appimagetool-823-x86_64.AppImage
+wget https://github.com/probonopd/go-appimage/releases/download/continuous/appimaged-869-x86_64.AppImage
 wget https://download.opensuse.org/repositories/home:/Alexx2000/AppImage/doublecmd-qt-latest-x86_64.AppImage
