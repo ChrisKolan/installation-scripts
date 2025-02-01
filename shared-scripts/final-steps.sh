70110bf5bb9a2569b3fb052edf13872374f3d4aa
chris <chris.kolann@gmail.com>
2025-02-01 17:29:42 +0100

    added some delays

--- shared-scripts/final-steps.sh
+++ shared-scripts/final-steps.sh
@@ -3,9 +3,11 @@
 echo "Disabling anonymous analytics in Homebrew"
 source ~/.bashrc
 brew analytics off
+sleep 3s
 
 echo "Opening browser for Syncting and GitHub configuration"
 xdg-open http://127.0.0.1:8384
+sleep 5s
 xdg-open https://github.com/settings/keys
 sleep 3s
 
