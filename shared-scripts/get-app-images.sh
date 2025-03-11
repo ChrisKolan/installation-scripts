c652a4df25de7e2605756dfdf45af4b5354c201d
Chris Kolan <chris.kolann@gmail.com>
2025-03-11 22:19:49 +0100

    Update get-app-images.sh
    Added rustdesk AppImage

--- shared-scripts/get-app-images.sh
+++ shared-scripts/get-app-images.sh
@@ -5,3 +5,4 @@ wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.8.4/Ob
 wget https://github.com/keepassxreboot/keepassxc/releases/download/2.7.9/KeePassXC-2.7.9-x86_64.AppImage
 wget https://github.com/probonopd/go-appimage/releases/download/continuous/appimaged-869-x86_64.AppImage
 wget https://download.opensuse.org/repositories/home:/Alexx2000/AppImage/doublecmd-qt-latest-x86_64.AppImage
+wget https://github.com/rustdesk/rustdesk/releases/download/1.3.8/rustdesk-1.3.8-x86_64.AppImage
