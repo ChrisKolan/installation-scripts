825d4eaca6c28d3435a8e398fc5977b963f54fe2
Chris Kolan <chris.kolann@gmail.com>
2025-03-31 15:03:00 +0200

    Update install-flatpaks.sh

--- shared-scripts/install-flatpaks.sh
+++ shared-scripts/install-flatpaks.sh
@@ -6,3 +6,4 @@ flatpak install --user -y flathub org.freeciv.gtk322
 flatpak install --user -y flathub io.gitlab.librewolf-community
 flatpak install --user -y flathub org.musicbrainz.Picard
 flatpak install --user -y flathub org.fooyin.fooyin
+flatpak install --user -y flathub com.calibre_ebook.calibre
