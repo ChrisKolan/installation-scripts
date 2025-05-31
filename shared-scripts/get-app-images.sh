340d2b264693d07519d4c4de6ac0fee7c9bf75c3
Chris Kolan <chris.kolann@gmail.com>
2025-05-31 21:36:59 +0200

    Update get-app-images.sh
    updated AppImage versions

--- shared-scripts/get-app-images.sh
+++ shared-scripts/get-app-images.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd ~/bin
-wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.8.4/Obsidian-1.8.4.AppImage
-wget https://github.com/keepassxreboot/keepassxc/releases/download/2.7.9/KeePassXC-2.7.9-x86_64.AppImage
-wget https://github.com/probonopd/go-appimage/releases/download/continuous/appimaged-869-x86_64.AppImage
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.8.10/Obsidian-1.8.10.AppImage
+wget https://github.com/keepassxreboot/keepassxc/releases/download/2.7.10/KeePassXC-2.7.10-x86_64.AppImage
+wget https://github.com/probonopd/go-appimage/releases/download/continuous/appimaged-886-x86_64.AppImage
+wget https://github.com/rustdesk/rustdesk/releases/download/1.4.0/rustdesk-1.4.0-x86_64.AppImage
 wget https://download.opensuse.org/repositories/home:/Alexx2000/AppImage/doublecmd-qt-latest-x86_64.AppImage
-wget https://github.com/rustdesk/rustdesk/releases/download/1.3.8/rustdesk-1.3.8-x86_64.AppImage
