8bdd534aa899be4260c80eb25fbafbc6e2a69da9
Chris Kolan <chris.kolann@gmail.com>
2026-09-06 11:02:40 +0200

    Remove KeePassXC AppImage download
    Removed download link for KeePassXC AppImage.

--- shared-scripts/get-app-images.sh
+++ shared-scripts/get-app-images.sh
@@ -2,7 +2,6 @@
 
 cd ~/bin
 wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.8.10/Obsidian-1.8.10.AppImage
-wget https://github.com/keepassxreboot/keepassxc/releases/download/2.7.10/KeePassXC-2.7.10-x86_64.AppImage
 wget https://github.com/probonopd/go-appimage/releases/download/continuous/appimaged-886-x86_64.AppImage
 wget https://github.com/rustdesk/rustdesk/releases/download/1.4.0/rustdesk-1.4.0-x86_64.AppImage
 wget https://download.opensuse.org/repositories/home:/Alexx2000/AppImage/doublecmd-qt-latest-x86_64.AppImage
